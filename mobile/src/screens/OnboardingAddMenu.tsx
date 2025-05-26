c6a0c2263dc0d0f57591058f7c26f23d1a2cb8fe
TosaOdiase <140124217+TosaOdiase@users.noreply.github.com>
2025-05-26 15:20:50 -0500

    Update OnboardingAddMenu.tsx

--- mobile/src/screens/OnboardingAddMenu.tsx
+++ mobile/src/screens/OnboardingAddMenu.tsx
@@ -694,7 +694,7 @@ const styles = StyleSheet.create({
   },
   continueButton: {
     position: 'absolute',
-    bottom: 120,
+    bottom: 20,
     left: 0,
     right: 0,
     alignItems: 'center',
